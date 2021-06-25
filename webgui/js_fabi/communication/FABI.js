cdba58797f5a72f61f7dc0187bd64b96b40e2588
Benjamin Klaus <klaus@technikum-wien.at>
2021-06-25 11:26:19 +0200

    added FABI version of configuration

---
+++ webgui/js_fabi/communication/FABI.js
@@ -0,0 +1,67 @@
+import {ATDevice} from "../../js/communication/ATDevice.js";
+
+let FABI = {};
+FABI.LIVE_PRESSURE = 'LIVE_PRESSURE';
+FABI.LIVE_PRESSURE_MIN = 'LIVE_PRESSURE_MIN';
+FABI.LIVE_PRESSURE_MAX = 'LIVE_PRESSURE_MAX';
+FABI.LIVE_BUTTONS = 'LIVE_BUTTONS';
+
+let _liveData = {};
+let _liveValueHandler = null;
+let _lastLiveValueParse = 0;
+
+FABI.startLiveValueListener = function (handler) {
+    _liveValueHandler = handler;
+};
+
+FABI.stopLiveValueListener = function () {
+    _liveValueHandler = null;
+};
+
+FABI.getLiveData = function (constant) {
+    if (constant) {
+        return _liveData[constant];
+    }
+    return _liveData;
+};
+
+FABI.resetMinMaxLiveValues = function () {
+    _liveData[FABI.LIVE_PRESSURE_MIN] = 1024;
+    _liveData[FABI.LIVE_PRESSURE_MAX] = -1;
+};
+
+function parseLiveData(data) {
+    if (Object.keys(_liveData).length === 0) {
+        FABI.resetMinMaxLiveValues()
+    }
+    if (!data) {
+        return;
+    }
+
+    let interval = _liveValueHandler ? 0 : 300;
+    if (new Date().getTime() - _lastLiveValueParse > interval) {
+        _lastLiveValueParse = new Date().getTime();
+        let valArray = data.split(':')[1].split(',');
+        _liveData[FABI.LIVE_PRESSURE] = parseInt(valArray[0]);
+        if (valArray[1]) {
+            _liveData[FABI.LIVE_BUTTONS] = valArray[1].split('').map(v => v === "1");
+        }
+        if (valArray[2]) {
+            let slot = ATDevice.getSlotName(parseInt(valArray[2]));
+            if (slot && slot !== ATDevice.getCurrentSlot()) {
+                ATDevice.setSlot(slot, true);
+            }
+        }
+        _liveData[FABI.LIVE_PRESSURE_MIN] = Math.min(_liveData[FABI.LIVE_PRESSURE_MIN], _liveData[FABI.LIVE_PRESSURE]);
+        _liveData[FABI.LIVE_PRESSURE_MAX] = Math.max(_liveData[FABI.LIVE_PRESSURE_MAX], _liveData[FABI.LIVE_PRESSURE]);
+
+        if (_liveValueHandler) {
+            _liveValueHandler(_liveData);
+        }
+    }
+}
+
+ATDevice.setLiveValueHandler(parseLiveData);
+
+export {FABI};
+export default FABI;
--- webgui/js_fabi/constantsFabi.js
+++ webgui/js_fabi/constantsFabi.js
@@ -1,3 +1,7 @@
+import {TabSlots} from "../js/ui/views/TabSlots.js";
+import {TabActions} from "../js/ui/views/TabActions.js";
+import {TabGeneral} from "../js/ui/views/TabGeneral.js";
+
 window.C = window.C || {};
 
 C.CURRENT_DEVICE = C.AT_DEVICE_FABI;
@@ -6,3 +10,71 @@ C.DEVICE_IS_FM = false;
 C.USB_DEVICE_FILTERS =  [
     {usbVendorId: 0x2341} // Arduino Pro Micro
 ];
+
+C.VIEWS = [{
+    object: TabActions,
+    hash: '#tabActions',
+    label: 'Actions // Aktionen'
+}, {
+    object: TabSlots,
+    hash: '#tabSlots',
+    label: 'Slots // Slots'
+}, {
+    object: TabGeneral,
+    hash: '#tabGeneral',
+    label: 'General // Allgemein'
+}];
+
+C.AT_CMD_CATEGORIES = [{
+    constant: C.AT_CMD_CAT_KEYBOARD,
+    label: 'Keyboard // Tastatur'
+}, {
+    constant: C.AT_CMD_CAT_MOUSE,
+    label: 'Mouse // Maus'
+}, {
+    constant: C.AT_CMD_CAT_JOYSTICK,
+    label: 'Joystick'
+}, {
+    constant: C.AT_CMD_CAT_DEVICE,
+    label: 'Device // Gerät'
+}];
+
+C.BTN_CAT_BTN = 'BTN_CAT_BTN';
+C.BTN_CATEGORIES = [{
+    constant: C.BTN_CAT_BTN,
+    label: 'Buttons'
+}]
+
+C.BTN_MODES = [{
+    index: 1,
+    label: 'Button 1',
+    category: C.BTN_CAT_BTN
+}, {
+    index: 2,
+    label: 'Button 2',
+    category: C.BTN_CAT_BTN
+}, {
+    index: 3,
+    label: 'Button 3',
+    category: C.BTN_CAT_BTN
+}, {
+    index: 4,
+    label: 'Button 4',
+    category: C.BTN_CAT_BTN
+}, {
+    index: 5,
+    label: 'Button 5',
+    category: C.BTN_CAT_BTN
+}, {
+    index: 6,
+    label: 'Button 6',
+    category: C.BTN_CAT_BTN
+}, {
+    index: 7,
+    label: 'Button 7',
+    category: C.BTN_CAT_BTN
+}, {
+    index: 8,
+    label: 'Button 8',
+    category: C.BTN_CAT_BTN
+}, ];
