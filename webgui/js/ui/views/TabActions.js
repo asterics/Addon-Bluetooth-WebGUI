8c1736d88f580ff61d79659f845aea4922008737
Benjamin Klaus <klaus@technikum-wien.at>
2021-07-15 13:54:08 +0200

    switch between mobile/desktop view based on current window size in em and number of slots

--- webgui/js/ui/views/TabActions.js
+++ webgui/js/ui/views/TabActions.js
@@ -15,12 +15,23 @@ class TabActions extends Component {
         this.state = {
             showCategory: null,
             modalBtnMode: null,
-            modalSlot: null
+            modalSlot: null,
+            widthEm: window.innerWidth / parseFloat(getComputedStyle(document.querySelector('body'))['font-size'])
         }
+        window.addEventListener('resize', this.onresize);
     }
 
     componentWillUnmount() {
         L.removeClass('body', 'modal-open');
+        window.removeEventListener('resize', this.onresize);
+    }
+
+    onresize() {
+        L.debounce(() => {
+            TabActions.instance.setState({
+                widthEm: window.innerWidth / parseFloat(getComputedStyle(document.querySelector('body'))['font-size'])
+            });
+        }, 200, 'RESIZE_TAB_ACTIONS')
     }
 
     getLinkTitle(btnMode, slot) {
@@ -48,10 +59,25 @@ class TabActions extends Component {
         let count = ATDevice.getSlots().length;
         return count > 1 && ATDevice.getCurrentSlot() === slot ? 'font-weight-bold' : '';
     }
+
+    getMaxPrintableSlots() {
+        let em = this.state.widthEm;
+        if (em >= 149) return 8;
+        if (em >= 132) return 7;
+        if (em >= 116) return 6;
+        if (em >= 100) return 5;
+        if (em >= 84) return 4;
+        if (em >= 67) return 3;
+        if (em >= 51) return 2;
+        if (em >= 35) return 1;
+        return 0;
+    }
     
     render() {
         let state = this.state;
         let slots = ATDevice.getSlots();
+        let mobileView = slots.length > this.getMaxPrintableSlots();
+
         let btnModes = C.BTN_MODES.filter(mode => !this.state.showCategory || mode.category === this.state.showCategory);
         let modalOpen = !!state.modalBtnMode;
         if(modalOpen) {
@@ -64,21 +90,21 @@ class TabActions extends Component {
 
         return html`<div id="tabActions">
              <h2>${L.translate('Action configuration // Aktionen-Konfiguration')}</h2>
-             <div class="d-md-none filter-buttons mb-3">
+             <div class="${mobileView ? '' : 'd-none'} filter-buttons mb-3">
                 ${html`<${RadioFieldset} legend="Show categories: // Zeige Kategorien:" onchange="${(value) => this.setState({showCategory: value})}" elements="${categoryElements}" value="${state.showCategory}"/>`}
              </div>
              
              <ul>
-                <li class="row d-none d-md-flex" aria-hidden="true" style="font-style: italic">
-                    <span class="col-12 col-md">Bezeichnung</span>
-                    ${slots.map(slot => html`<span class="col-12 col-md ${this.getSlotStyle(slot)}">Slot "${slot}"</span>`)}
+                <li class="row ${mobileView ? 'd-none' : 'd-flex'}" aria-hidden="true" style="font-style: italic">
+                    <span class="${mobileView ? 'col-12' : 'col'}">Bezeichnung</span>
+                    ${slots.map(slot => html`<span class="${mobileView ? 'col-12' : 'col'} ${this.getSlotStyle(slot)}">Slot "${slot}"</span>`)}
                 </li>
                 ${btnModes.map((btnMode, index) => html`
-                    <li class="row py-3 py-md-0" style="${index % 2 === 1 ? 'background-color: lightgray' : ''}">
-                        <strong class="col-12 col-md">${L.translate(btnMode.label)}</strong>
+                    <li class="row ${mobileView ? 'py-3' : 'py-0'}" style="${index % 2 === 1 ? 'background-color: lightgray' : ''}">
+                        <strong class="${mobileView ? 'col-12' : 'col'}">${L.translate(btnMode.label)}</strong>
                         ${slots.map(slot => html`
-                            <span class="col-12 col-md ${this.getSlotStyle(slot)}">
-                                <span class="d-md-none">Slot "${slot}": </span>
+                            <span class="${mobileView ? 'col-12' : 'col'} ${this.getSlotStyle(slot)}">
+                                <span class="${mobileView ? '' : 'd-none'}">Slot "${slot}": </span>
                                 <a href="javascript:;" title="${this.getLinkTitle(btnMode, slot)}" onclick="${() => this.setState({modalBtnMode: btnMode, modalSlot: slot})}">${this.getLinkLabel(btnMode, slot)}</a>
                             </span>
                         `)}
