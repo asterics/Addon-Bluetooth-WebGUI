c4799e1b693c20f8468659d6f722e7649a7c58cd
Benjamin Klaus <klaus@technikum-wien.at>
2021-08-11 09:25:17 +0200

    added component for fa-icons

--- webgui/js/ui/components/FaIcon.js
+++ webgui/js/ui/components/FaIcon.js
@@ -2,37 +2,15 @@ import { h, Component } from '../../../lib/preact.min.js';
 import htm from '../../../lib/htm.min.js';
 const html = htm.bind(h);
 
-class ActionButton extends Component {
-
-    constructor(props) {
-        super();
-        this.state = {
-            actionPerforming: false
-        }
-        this.props = props;
-    }
-
-    doAction() {
-        this.props.onclick = this.props.onclick || (() => {
-        });
-        this.setState({
-            actionPerforming: true
-        });
-        Promise.resolve(this.props.onclick()).then(() => {
-            this.setState({
-                actionPerforming: false
-            });
-        })
-    }
+class FaIcon extends Component {
 
     render(props) {
-        props.label = props.label || '';
-        props.progressLabel = props.progressLabel || this.props.label;
-        return html`
-            <button onclick="${() => this.doAction()}" disabled="${this.state.actionPerforming}">
-                ${L.translate(this.state.actionPerforming ? props.progressLabel : props.label)}
-            </button>`;
+        props.icon = props.icon || '';
+        let folder = props.icon.split(' ')[0];
+        let icon = props.icon.split(' ')[1];
+        let path = `./lib/fa-svgs/${folder}/${icon}.svg`;
+        return html`<img src="${path}" style="display: inline-block; height: 1.2em; margin: 0 0.5em; vertical-align: middle"/>`;
     }
 }
 
-export {ActionButton};
+export {FaIcon};
