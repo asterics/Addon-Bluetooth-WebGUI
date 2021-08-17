ec8f9010e6e222cdfd55319dc0b89bd139d10242
Benjamin Klaus <klaus@technikum-wien.at>
2021-08-17 13:53:24 +0200

    added localStorageService.js

---
+++ webgui/js/localStorageService.js
@@ -0,0 +1,42 @@
+let localStorageService = {};
+let KEY_FRIMWARE_DOWNLOAD_URL = 'FMFABI_WEBGUI_KEY_FRIMWARE_DOWNLOAD_URL';
+let storage = null;
+
+if (typeof (Storage) !== "undefined") {
+    try {
+        storage = window.localStorage;
+    } catch (e) {
+        log.error('could not access local storage, maybe disabled by user? Error: ' + e)
+    }
+
+}
+
+localStorageService.save = function (key, value) {
+    if (storage) {
+        try {
+            return storage.setItem(key, value);
+        } catch (e) {
+            log.error(e)
+        }
+    }
+};
+
+localStorageService.get = function (key) {
+    if (storage) {
+        try {
+            return storage.getItem(key);
+        } catch (e) {
+            log.error(e)
+        }
+    }
+};
+
+localStorageService.getFirmwareDownloadUrl = function () {
+    return localStorageService.get(KEY_FRIMWARE_DOWNLOAD_URL) || "";
+};
+
+localStorageService.setFirmwareDownloadUrl = function (downloadUrl) {
+    return localStorageService.save(KEY_FRIMWARE_DOWNLOAD_URL, downloadUrl);
+};
+
+export {localStorageService};
