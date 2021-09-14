eb90280a42382caa4d05836c35531fbb6092c5b9
Benjamin Klaus <klaus@technikum-wien.at>
2021-09-14 16:45:13 +0200

    added possibility to change size of fa-icon

--- webgui/js/ui/components/FaIcon.js
+++ webgui/js/ui/components/FaIcon.js
@@ -8,12 +8,13 @@ class FaIcon extends Component {
         props.icon = props.icon || '';
         let folder = props.icon.split(' ')[0];
         let icon = props.icon.split(' ')[1];
+        let height = props.height || '1.2em';
         let path = `./lib/fa-svgs/${folder}/${icon}.svg`;
         if (!this.props.icon) {
             return '';
         }
         return html`<img src="${path}" aria-hidden="true"
-                         style="display: inline-block; height: 1.2em; margin: 0 0.5em; vertical-align: middle; ${this.props.invert ? 'filter: invert(1);' : ''}"/>`;
+                         style="${`display: inline-block; height: ${height}; margin: 0 0.5em; vertical-align: middle;`} ${this.props.invert ? 'filter: invert(1);' : ''}"/>`;
     }
 }
 
