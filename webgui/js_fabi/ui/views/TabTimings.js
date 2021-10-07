ce05da2758fd0376d777be885e78a6cd80e73bf6
Benjamin Klaus <klaus@technikum-wien.at>
2021-10-07 13:05:07 +0200

    changed order of options for timings, added steps

--- webgui/js_fabi/ui/views/TabTimings.js
+++ webgui/js_fabi/ui/views/TabTimings.js
@@ -38,32 +38,32 @@ class TabTimings extends Component {
 
         return html`
             <h2>${L.translate('Timings configuration (Slot "{?}") // Timing-Konfiguration (Slot "{?}")', ATDevice.getCurrentSlot())}</h2>
-            <h3>${L.translate('Antitremor settings // Antitremor-Einstellungen')}</h3>
-            <div class="mb-5 mb-md-2">
-                ${html`<${Slider} label="Antitremor time for press: // Antitremor Schwellenwert für Drücken:" oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_ANTITREMOR_PRESS]}"
-                                  min="1" max="500" updateConstants="${[C.AT_CMD_ANTITREMOR_PRESS]}"/>`}
-            </div>
-            <div class="mb-5 mb-md-2">
-                ${html`<${Slider} label="Antitremor time for release: // Antitremor Schwellenwert für Loslassen:" oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_ANTITREMOR_RELEASE]}"
-                                  min="1" max="500" updateConstants="${[C.AT_CMD_ANTITREMOR_RELEASE]}"/>`}
-            </div>
-            <div class="mb-5 mb-md-2">
-                ${html`<${Slider} label="Antitremor idle time: // Antitremor Wartezeit:" oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_ANTITREMOR_IDLE]}"
-                        min="1" max="500" updateConstants="${[C.AT_CMD_ANTITREMOR_IDLE]}"/>`}
-            </div>
-            
             <h3 class="mt-5">${L.translate('Timings for special functions // Schwellenwerte für Spezialfunktionen')}</h3>
             <div class="mb-5 mb-md-2">
                 ${html`<${Slider} label="Threshold for long press [ms], 0=disalbe: // Schwellenwert für langes Drücken [ms], 0=deaktivieren:" oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_THRESHOLD_LONGPRESS]}"
-                                  min="0" max="9999" updateConstants="${[C.AT_CMD_THRESHOLD_LONGPRESS]}"/>`}    
+                                  min="0" max="10000" step="100" updateConstants="${[C.AT_CMD_THRESHOLD_LONGPRESS]}"/>`}
             </div>
             <div class="mb-5 mb-md-2">
                 ${html`<${Slider} label="Threshold for slot change by double press [ms], 0=disalbe: // Schwellenwert für Slot-Weiterschalten durch doppeltes Drücken [ms], 0=deaktivieren:" oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_THRESHOLD_DOUBLEPRESS]}"
-                                  min="0" max="9999" updateConstants="${[C.AT_CMD_THRESHOLD_DOUBLEPRESS]}"/>`}     
+                                  min="0" max="10000" step="100" updateConstants="${[C.AT_CMD_THRESHOLD_DOUBLEPRESS]}"/>`}
             </div>
             <div class="mb-5 mb-md-2">
                 ${html`<${Slider} label="Threshold automatic left click [ms], 0=disalbe: // Schwellenwert für automatischen Linksklick [ms], 0=deaktivieren:" oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_THRESHOLD_AUTODWELL]}"
-                                  min="0" max="5000" updateConstants="${[C.AT_CMD_THRESHOLD_AUTODWELL]}"/>`}
+                                  min="0" max="5000" step="100" updateConstants="${[C.AT_CMD_THRESHOLD_AUTODWELL]}"/>`}
+            </div>
+            
+            <h3>${L.translate('Antitremor settings // Antitremor-Einstellungen')}</h3>
+            <div class="mb-5 mb-md-2">
+                ${html`<${Slider} label="Antitremor time for press [ms]: // Antitremor Schwellenwert für Drücken [ms]:" oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_ANTITREMOR_PRESS]}"
+                                  min="1" max="500" updateConstants="${[C.AT_CMD_ANTITREMOR_PRESS]}"/>`}
+            </div>
+            <div class="mb-5 mb-md-2">
+                ${html`<${Slider} label="Antitremor time for release [ms]: // Antitremor Schwellenwert für Loslassen [ms]:" oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_ANTITREMOR_RELEASE]}"
+                                  min="1" max="500" updateConstants="${[C.AT_CMD_ANTITREMOR_RELEASE]}"/>`}
+            </div>
+            <div class="mb-5 mb-md-2">
+                ${html`<${Slider} label="Antitremor idle time [ms]: // Antitremor Wartezeit [ms]:" oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_ANTITREMOR_IDLE]}"
+                        min="1" max="500" updateConstants="${[C.AT_CMD_ANTITREMOR_IDLE]}"/>`}
             </div>
             
             <div class="row" style="margin-top: 4em">
