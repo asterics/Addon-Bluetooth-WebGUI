f3ccc54faa99365b70a528e8c90ead3d3688fb94
Chris Veigl <veigl@technikum-wien.at>
2025-07-14 21:29:02 +0200

    removed currently unsupported features

--- webgui/js_new/ui/views/TabTimings.js
+++ webgui/js_new/ui/views/TabTimings.js
@@ -50,32 +50,25 @@ class TabTimings extends Component {
             <h2>${L.translate('Timings configuration (Slot "{?}") // Timing-Konfiguration (Slot "{?}")', ATDevice.getCurrentSlot())}
             </h2>
             <h3 class="mt-5">${L.translate('Timings for special functions // Schwellenwerte für Spezialfunktionen')}</h3>
-            
-            <div class="mb-5 mb-md-2">
-                ${html`
-                <${Slider}
-                    id="slider" label="Threshold for long press [ms], 0=disable: // Schwellenwert für langes Drücken [ms], 0=deaktivieren:"
-                    oninput="${(value, constants) => this.valueChanged(value, constants)}"
-                    value="${state[C.AT_CMD_THRESHOLD_LONGPRESS]}" min="0" max="10000" step="200"
-                    updateConstants="${[C.AT_CMD_THRESHOLD_LONGPRESS]}" />
-                `} 
-            </div>
 
-            <div class="mb-5 mb-md-2">
-                ${html`
-                    <${Slider} label="Threshold automatic left click [ms], 0=disable: // Schwellenwert für automatischen Linksklick [ms], 0=deaktivieren:" 
-                    oninput="${(value, constants) => this.valueChanged(value, constants)}" value="${state[C.AT_CMD_THRESHOLD_AUTODWELL]}"
-                                    min="0" max="5000" step="100" updateConstants="${[C.AT_CMD_THRESHOLD_AUTODWELL]}"/>
-                `}
-            </div>
-                
                 <!-- These settings not supported by now in the unified GUI version, TBD. 
+
                     <div class="mb-5 mb-md-2">
                         ${html`
-                            <${Slider} id="slider" label="Threshold for slot change by double press [ms], 0=disable: // Schwellenwert für Slot-Weiterschalten durch doppeltes Drücken [ms], 0=deaktivieren:" 
+                        <${Slider}
+                            id="slider" label="Threshold for long press [ms], 0=disable: // Schwellenwert für langes Drücken [ms], 0=deaktivieren:"
+                            oninput="${(value, constants) => this.valueChanged(value, constants)}"
+                            value="${state[C.AT_CMD_THRESHOLD_LONGPRESS]}" min="0" max="10000" step="200"
+                            updateConstants="${[C.AT_CMD_THRESHOLD_LONGPRESS]}" />
+                        `} 
+                    </div>
+                
+                    <div class="mb-5 mb-md-2">
+                        ${html`
+                            <${Slider} id="slider" label="Threshold for multiple button presses [ms], 0=disable: // Schwellenwert für mehrfaches Drücken [ms], 0=deaktivieren:" 
                             oninput="${(value, constants) => this.valueChanged(value, constants)}" 
-                            value="${state[C.AT_CMD_THRESHOLD_DOUBLEPRESS]}" min="0" max="10000" step="100" 
-                            updateConstants="${[C.AT_CMD_THRESHOLD_DOUBLEPRESS]}"/>`}
+                            value="${state[C.AT_CMD_THRESHOLD_MULTIPRESS]}" min="0" max="10000" step="100" 
+                            updateConstants="${[C.AT_CMD_THRESHOLD_MULTIPRESS]}"/>`}
                     </div>
 
                     <h3>${L.translate('Antitremor settings // Antitremor-Einstellungen')}</h3>
