dc21333c3f30cb00d7a54ec64dfbf93362aa5f51
Chris Veigl <veigl@technikum-wien.at>
2025-07-23 14:47:25 +0200

    udpated firmware lookup for different devices

--- webgui/js_new/util/firmwareUtil.js
+++ webgui/js_new/util/firmwareUtil.js
@@ -13,17 +13,32 @@ firmwareUtil.wait = function (milliseconds) {
     })
 }
 
+firmwareUtil.extractBoardValue = function (versionSuffix) {
+    const match = versionSuffix.match(/Board=([^\s,]+)/);
+    return match ? match[1].trim() : null;
+}
+
 
 firmwareUtil.getBTFWInfo = function () {
     return getFWInfo('https://api.github.com/repos/asterics/esp32_mouse_keyboard/releases/latest', '.bin');  // TBD: use .fm3 for new firmware versions?
 }
 
 firmwareUtil.getDeviceFWInfo = function (device, majorVersion) {
-    // TBD: handle new firmware versions for different RP2040 devices!
-    let repoName = C.CURRENT_DEVICE;
-    let fileType = '.uf2';
-    let fwInfo = getFWInfo(`https://api.github.com/repos/asterics/${repoName}/releases/latest`, '.uf2');  
-    console.log('firmwareUtil.getDeviceFWInfo: ', repoName, fwInfo);
+
+    let assetPostfix = C.CURRENT_DEVICE;
+    let fwURL = 'https://api.github.com/repos/asterics/FabiWare/releases/latest';
+    let boardValue = firmwareUtil.extractBoardValue(ATDevice.getVersionSuffix());
+    if (boardValue === 'Raspberry_Pi_Pico_2W' || boardValue === 'Raspberry_Pi_Pico_2') {
+        assetPostfix += '_RP2350.uf2';
+    } else if (boardValue === 'Raspberry_Pi_Pico_W' || boardValue === 'Raspberry_Pi_Pico') {
+        assetPostfix += '_RP2040.uf2';
+    } else if (boardValue === 'ARDUINO_NANO_RP2040_CONNECT') {
+        assetPostfix = 'FLIPMOUSE.uf2';
+    }
+    console.log('Microcontroller board=', boardValue );
+    console.log('Release asset API URL='+ fwURL + ', Asset filter=' + assetPostfix);
+ 
+     let fwInfo = getFWInfo(fwURL, assetPostfix);  
     return fwInfo;  
 }
 
@@ -50,12 +65,16 @@ firmwareUtil.updateDeviceFirmware = function (progressHandler) {
 
 function getFWInfo(apiUrl, binaryStringFilter) {
     return L.HTTPRequest(apiUrl, 'GET', 'json').then(result => {
-        let binaryAsset = result.assets.filter(asset => asset.name.indexOf(binaryStringFilter) > -1)[0];
+        let binaryAssets = result.assets;
+        let filteredAsset= binaryAssets.filter(asset => asset.name.indexOf(binaryStringFilter) > -1)[0];
+        console.log('Firmware assets:', binaryAssets);        
+        console.log('Filtered asset:', filteredAsset);
+
         return {
             version: L.formatVersion(result['tag_name']),
             infoUrl: result['html_url'],
-            downloadUrl: 'https://proxy.asterics-foundation.org/proxybase64url.php?csurl=' + encodeURIComponent(btoa(binaryAsset.browser_download_url)),
-            originalDownloadUrl: binaryAsset.browser_download_url
+            downloadUrl: 'https://proxy.asterics-foundation.org/proxybase64url.php?csurl=' + encodeURIComponent(btoa(filteredAsset.browser_download_url)),
+            originalDownloadUrl: filteredAsset.browser_download_url
         };
     });
 }
